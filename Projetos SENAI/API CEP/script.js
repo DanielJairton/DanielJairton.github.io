869938d5aa91cde3ce6bfb0e39bc0e7da53e696e
DanielJairton <daniel.jairton679@gmail.com>
2023-12-10 12:56:03 -0300

    Teste

---
+++ Projetos SENAI/API CEP/script.js
@@ -0,0 +1,18 @@
+
+function pesquisar(){
+    let cep = document.getElementById('input-cep').value;
+    dados(cep)
+}
+
+async function dados(cep){
+    let dados = await fetch(`https://viacep.com.br/ws/${cep}/json/`).then(Response => Response.json())
+    inserirDados(dados)
+}
+
+function inserirDados(dados){
+    document.getElementById('input-endereco').value = dados.logradouro;
+    document.getElementById('input-bairro').value = dados.bairro;
+    document.getElementById('input-cidade').value = dados.localidade;
+    document.getElementById('input-estado').value = dados.uf;
+}
+
