b3aecd29d6e8e7246224fa172730c50181b63b11
DanielJairton <daniel.jairton679@gmail.com>
2023-12-20 22:03:00 -0300

    Calculadora de depreciação - versão final

--- Projetos SENAI/Calculadora de depreciação/assets/js/main.js
+++ Projetos SENAI/Calculadora de depreciação/assets/js/main.js
@@ -38,5 +38,9 @@ document.getElementById('btn-calcular').addEventListener('click', function(){
     }
 
     //Calculo
+    document.getElementById('input-depreciacao').value = ((valorTotal.value-valorSucata.value)/vidaUtil.value)*(tempoUso.value/12);
+    document.getElementById('input-valorContabil').value = valorTotal.value-(((valorTotal.value-valorSucata.value)/vidaUtil.value)*(tempoUso.value/12));
+    document.getElementById('input-depreciacaoAnual').value = (valorTotal.value-valorSucata.value)/vidaUtil.value;
+    document.getElementById('input-depreciacaoMensal').value = ((valorTotal.value-valorSucata.value)/vidaUtil.value)/12;
     
 })
