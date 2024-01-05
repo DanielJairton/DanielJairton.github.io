39d5251849266710a98ce07a342ee7928dcb87a8
DanielJairton <daniel.jairton679@gmail.com>
2024-01-05 10:15:59 -0300

    update

---
+++ Projetos SENAI/Projeto Surpresa/Delivery Fastfood/assets/js/dados.js
@@ -0,0 +1,62 @@
+const  tipoBebida = [
+    {
+        "idTipoBebida": 1,
+        "tipoBebida": "Água Mineral",
+    },
+    {
+        "idTipoBebida": 2,
+        "tipoBebida": "Refrigerante",
+    },
+    {
+        "idTipoBebida": 3,
+        "tipoBebida": "Suco",
+    }
+]
+
+const bebida = [
+    {
+        "idTipoBebida": 1,
+        "tamanho": "500ml",
+        "precoUn": 2
+    },
+    {
+        "idTipoBebida": 1,
+        "tamanho": "1L",
+        "precoUn": 4
+    },
+    {
+        "idTipoBebida": 1,
+        "tamanho": "2L",
+        "precoUn": 7
+    },
+    {
+        "idTipoBebida": 2,
+        "tamanho": "500ml",
+        "precoUn": 5
+    },
+    {
+        "idTipoBebida": 2,
+        "tamanho": "1L",
+        "precoUn": 9
+    },
+    {
+        "idTipoBebida": 2,
+        "tamanho": "2L",
+        "precoUn": 13
+    },
+    {
+        "idTipoBebida": 3,
+        "tamanho": "500ml",
+        "precoUn": 6
+    },
+    {
+        "idTipoBebida": 3,
+        "tamanho": "1L",
+        "precoUn": 14
+    },
+    {
+        "idTipoBebida": 3,
+        "tamanho": "1,5L",
+        "precoUn": 18
+    }
+]
