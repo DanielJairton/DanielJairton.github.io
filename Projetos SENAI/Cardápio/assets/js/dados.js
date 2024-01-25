e6ed65a2ab04b97bdb2f9ec31d92941f70ccbd13
DanielJairton <daniel.jairton679@gmail.com>
2024-01-25 19:36:00 -0300

    update

---
+++ Projetos SENAI/Cardápio/assets/js/dados.js
@@ -0,0 +1,155 @@
+const dadosAlmoco = [
+    {
+        "id": 0,
+        "titulo": "Arroz com feijão e frango grelhado",
+        "preco": 10,
+        "imgUrl": "./assets/img/almoco.svg"
+    },
+    {
+        "id": 1,
+        "titulo": "Feijoada",
+        "preco": 20,
+        "imgUrl": "./assets/img/almoco.svg"
+    },
+    {
+        "id": 2,
+        "titulo": "Prato Vegano",
+        "preco": 20,
+        "imgUrl": "./assets/img/almoco.svg"
+    },
+    {
+        "id": 3,
+        "titulo": "Macarronada",
+        "preco": 22,
+        "imgUrl": "./assets/img/almoco.svg"
+    },
+    {
+        "id": 4,
+        "titulo": "Feijão Tropeiro",
+        "preco": 20,
+        "imgUrl": "./assets/img/almoco.svg"
+    },
+    {
+        "id": 5,
+        "titulo": "Torta de legumes",
+        "preco": 20,
+        "imgUrl": "./assets/img/almoco.svg"
+    }
+]
+
+const dadosJantar = [
+    {
+        "id": 0,
+        "titulo": "Arroz com feijão e frango grelhado",
+        "preco": 10,
+        "imgUrl": "./assets/img/jantar.svg"
+    },
+    {
+        "id": 1,
+        "titulo": "Feijoada",
+        "preco": 20,
+        "imgUrl": "./assets/img/jantar.svg"
+    },
+    {
+        "id": 2,
+        "titulo": "Prato Vegano",
+        "preco": 20,
+        "imgUrl": "./assets/img/jantar.svg"
+    },
+    {
+        "id": 3,
+        "titulo": "Macarronada",
+        "preco": 22,
+        "imgUrl": "./assets/img/jantar.svg"
+    },
+    {
+        "id": 4,
+        "titulo": "Feijão Tropeiro",
+        "preco": 20,
+        "imgUrl": "./assets/img/jantar.svg"
+    },
+    {
+        "id": 5,
+        "titulo": "Torta de legumes",
+        "preco": 20,
+        "imgUrl": "./assets/img/jantar.svg"
+    }
+]
+
+const dadosBebidas = [
+    {
+        "id": 0,
+        "titulo": "Arroz com feijão e frango grelhado",
+        "preco": 10,
+        "imgUrl": "./assets/img/bebidas.svg"
+    },
+    {
+        "id": 1,
+        "titulo": "Feijoada",
+        "preco": 20,
+        "imgUrl": "./assets/img/bebidas.svg"
+    },
+    {
+        "id": 2,
+        "titulo": "Prato Vegano",
+        "preco": 20,
+        "imgUrl": "./assets/img/bebidas.svg"
+    },
+    {
+        "id": 3,
+        "titulo": "Macarronada",
+        "preco": 22,
+        "imgUrl": "./assets/img/bebidas.svg"
+    },
+    {
+        "id": 4,
+        "titulo": "Feijão Tropeiro",
+        "preco": 20,
+        "imgUrl": "./assets/img/bebidas.svg"
+    },
+    {
+        "id": 5,
+        "titulo": "Torta de legumes",
+        "preco": 20,
+        "imgUrl": "./assets/img/bebidas.svg"
+    }
+]
+
+const dadosSobremesa = [
+    {
+        "id": 0,
+        "titulo": "Arroz com feijão e frango grelhado",
+        "preco": 10,
+        "imgUrl": "./assets/img/sobremesa.svg"
+    },
+    {
+        "id": 1,
+        "titulo": "Feijoada",
+        "preco": 20,
+        "imgUrl": "./assets/img/sobremesa.svg"
+    },
+    {
+        "id": 2,
+        "titulo": "Prato Vegano",
+        "preco": 20,
+        "imgUrl": "./assets/img/sobremesa.svg"
+    },
+    {
+        "id": 3,
+        "titulo": "Macarronada",
+        "preco": 22,
+        "imgUrl": "./assets/img/sobremesa.svg"
+    },
+    {
+        "id": 4,
+        "titulo": "Feijão Tropeiro",
+        "preco": 20,
+        "imgUrl": "./assets/img/sobremesa.svg"
+    },
+    {
+        "id": 5,
+        "titulo": "Torta de legumes",
+        "preco": 20,
+        "imgUrl": "./assets/img/sobremesa.svg"
+    }
+]
---
+++ Projetos SENAI/Cardápio/assets/js/main.js
@@ -0,0 +1,22 @@
+
+function carregarMenus(dadosMenu, divSecao) {
+    // let dados = dadosMenu;
+
+    for (let index = 0; index < dadosMenu.length; index++) {
+        let cardBox = document.createElement('div');
+
+        cardBox.innerHTML = "<div class='cardBox'>" +
+        "<img src='" + dadosMenu[index].imgUrl + "' alt='item' class='cardImg'>" +
+        "<h4 class='cardTitulo'>"+ dadosMenu[index].titulo +"</h4>" +
+        "<span class='cardPreco'>" + dadosMenu[index].preco + "</span>" +
+        "</div>";
+
+        divSecao.appendChild(cardBox);
+    }
+
+}
+
+carregarMenus(dadosAlmoco, document.getElementById('box-almoco'));
+carregarMenus(dadosJantar, document.getElementById('box-jantar'));
+carregarMenus(dadosBebidas, document.getElementById('box-bebidas'));
+carregarMenus(dadosSobremesa, document.getElementById('box-sobremesa'));
